2465644eb2daf7dc59c2d5207e4ab4351bc87fc3
Alexander Liang <al5628@students.cs.ubc.ca>
2022-07-07 22:26:26 -0700

    remove all references to personal server from code, adding MySQL and reaction role removal soon

--- DBCharlie-Public.py
+++ DBCharlie-Public.py
@@ -6,84 +6,64 @@ import random
 from discord.ext.commands import bot
 
 
+
+REACTIONROLECHANNELID = None
+SERVERID = 0000000000
+WELCOMECHANNELID = 0000000
+DELETEDMESSAGECHANNEL = 000000000
+BOTTOKEN = ""
+
+
+
+
+
+
+SERVER = None
+
+
 @bot.event
 async def on_ready():
     print('Logged in as {0} ({0.id})'.format(bot.user))
-    await bot.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.listening, name="It's less broken"))
+    SERVER = discord.utils.get(bot.guilds, id=SERVERID)
+
+    # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="It's less broken"))
 
 
 @bot.event
 async def on_raw_reaction_add(payload):
-    if payload.channel_id == 757090693054201896:
-        Among_Us = discord.utils.get(payload.member.guild.roles, name="Among Us")
-        blue = discord.utils.get(payload.member.guild.roles, name="Blue")
-        green = discord.utils.get(payload.member.guild.roles, name="Green")
-        red = discord.utils.get(payload.member.guild.roles, name="Red")
-        orange = discord.utils.get(payload.member.guild.roles, name="Orange")
-        pink = discord.utils.get(payload.member.guild.roles, name="Pink")
-        yellow = discord.utils.get(payload.member.guild.roles, name="Yellow")
-        white = discord.utils.get(payload.member.guild.roles, name="White")
-        brown = discord.utils.get(payload.member.guild.roles, name="Brown")
-        cyan = discord.utils.get(payload.member.guild.roles, name="Cyan")
-        turquoise = discord.utils.get(payload.member.guild.roles, name="Turquoise")
+    if payload.channel_id == REACTIONROLECHANNELID:
 
-        role_dict = {"<:Orange:757310955817533521>": orange, "<:Red:757310294228861049>": red,
-                     "<:Green:757310606524153896>": green, "<:Blue:757310582213836850>": blue,
-                     "<:Among_Us:757479359471812710>": Among_Us, "<:Pink:759153160404598845>": pink,
-                     "<:Yellow:759157709596524595>": yellow, "<:White:759261673906634772>": white,
-                     "<:Brown:759261638288736306>": brown, "<:Cyan:759261608684814438>": cyan,
-                     "<:Turquoise:759603495896088596>": turquoise}
-        for role in role_dict:
-            if str(payload.emoji) == role:
-                await payload.member.add_roles(role_dict.get(role))
+        # custom emoji role adder
+        custom_emoji_role_dict = {999999: "role name", 00000000: "role name"}
+
+        for roleid in custom_emoji_role_dict:
+            if payload.emoji.id == roleid:
+                await payload.member.add_roles(discord.utils.get(SERVER.roles,name=custom_emoji_role_dict.get(roleid)))
                 break
 
-        if str(payload.emoji) == "<:RedX:757655095763533914>":
-            all_roles = [Among_Us, blue, green, red, orange, pink, yellow, white, brown, cyan, turquoise]
-            for role in all_roles:
-                await payload.member.remove_roles(role)
+        builtin_emoji_role_dict = {"emoji name": "role name", "emoji name": "role name",
+                     "emoji name": "role name"}
 
-    if payload.channel_id == 767146533476630588:
-        if payload.emoji.name == "❌":
-            for channel in payload.member.guild.voice_channels:
-                if len(channel.members) != 0:
-                    for member in channel.members:
-                        if channel.id != 767119448099782676:
-                            await member.edit(mute=True)
+        for roleid in builtin_emoji_role_dict:
+            if payload.emoji.name == roleid:
+                await payload.member.add_roles(discord.utils.get(SERVER.roles,name=builtin_emoji_role_dict.get(roleid)))
+                break
 
-    if payload.channel_id == 767146533476630588:
-        if payload.emoji.name == "🎙️":
-            for channel in payload.member.guild.voice_channels:
-                if len(channel.members) != 0:
-                    for member in channel.members:
-                        await member.edit(mute=False)
+
 
 
 
 @bot.event
 async def on_member_join(member):
 
-    channel = discord.utils.get(member.guild.channels, id=674455987188400140)
-    if member.id != 757311999997771776:
-        await channel.send("Welcome to the Edward's Official Only Fan's Discord Server " + member.mention +
-                           ". Please ensure that you have https://onlyfans.com/edwardshumongousstrips "
-                           "bookmarked for the opportunity "
-                           "to interact with the legendary Edward. "
-                           "If you require assistance please type `help`.")
+    channel = discord.utils.get(SERVER.channels, id=WELCOMECHANNELID)
+    await channel.send("")
 
 
 
 @bot.event
 async def on_message(message):
     print('Message from {0.author}: {0.content}'.format(message))
-    if message.content == "help":
-        channel = message.channel
-        await channel.send("How can I help you today?")
-        await asyncio.sleep(10)
-        await channel.send(
-            "Jokes on you, that's above my paygrade." + "However, <@272531524648173569> will be happy to assist "
-                                                        "you.")
 
     if message.content == "thisisastartupmessageandhopefullynoonetriggersit":
         embedVar = discord.Embed(title="This is where you obtain roles", description="", color=0x123456)
@@ -94,28 +74,10 @@ async def on_message(message):
         await message.channel.send(embed=embedVar)
 
 
-    if message.content.startswith("!among-us"):
-        code = message.content[9:]
-        for channel in message.author.guild.channels:
-            if str(channel) == "among-us-notifs":
-                embedVar = discord.Embed(title=" A New Among Us Game Has Started",
-                                         description="The Code is " + code, color=0x123456)
-                await channel.send(embed=embedVar)
-                await channel.send("<@&757092321442660493>")
 
-    if str(message.channel) == "talon-voice":
-        strtosend = message.content
-        await message.channel_mentions[0].send(strtosend[strtosend.index(' ') + 1:])
-
-    if message.channel.id == 766542511702933525:
-        to_kick = discord.utils.get(message.guild.members, display_name=message.content)
-        if to_kick != None:
-            await message.guild.kick(to_kick)
-            to_delete = message.channel
-            await to_delete.delete()
 
     if message.content.startswith("!create-command"):
-        if str(message.author) == "TalonM4#4077":
+        if message.author.id == SERVER.owner_id:
             processedOnce = message.content[message.content.index(" ") + 1:]
             processedDefinition = processedOnce[processedOnce.index(" ") + 1:]
             processedKey = processedOnce[: processedOnce.index(" "):]
@@ -145,63 +107,15 @@ async def on_message(message):
                 await message.channel.send(commandstd.get(key))
 
 
-    if message.content == "!mute-all":
-        if message.author.id == 272531524648173569:
-            for channel in message.author.guild.voice_channels:
-                if len(channel.members) != 0:
-                    for member in channel.members:
-                        await member.edit(mute=True)
-
-    if message.content == "!unmute-all":
-        if message.author.id == 272531524648173569:
-            for channel in message.author.guild.voice_channels:
-                if len(channel.members) != 0:
-                    for member in channel.members:
-                        await member.edit(mute=False)
-
-    if message.content == "!dead":
-        invc = False
-        for channel in message.author.guild.voice_channels:
-            for member in channel.members:
-                if member == message.author:
-                    invc = True
-        if invc:
-            moveTo = discord.utils.get(message.guild.voice_channels, id=767119448099782676)
-            await message.author.move_to(moveTo)
-        else:
-            await message.channel.send("You need to be in a VC.")
-
-    if message.content == "!seth":
-        await message.channel.send("https://cdn.discordapp.com/attachments/746092914668601344/795897568004407316/gYEcXI8h.png")
-
-        if random.randrange(100) == 4:
-            if message.author.dm_channel == None:
-                await message.author.create_dm()
-
-            await message.author.dm_channel.send("Roses are red"
-                                                 "\nviolets are blue"
-                                                 "\nyou don't like Seth"
-                                                 "\nso I'll kick you")
-
-            invite = await message.channel.create_invite(max_age = 120, max_uses = 1)
-
-            await message.author.dm_channel.send(invite.url)
-
-            await message.author.kick()
-
-
-    await bot.process_commands(message)
-
 
 @bot.event
 async def on_message_delete(message):
-    for channel in message.author.guild.channels:
-        if str(channel) == "deleted-messages":
-            if str(message.channel) != "deleted-messages":
-                await channel.send(str(message.author) + ": " + message.content)
-                for attachment in message.attachments:
-                    convert = await attachment.to_file(use_cached=True)
-                    await channel.send(file=convert)
+    deleted_message_channel = discord.utils.get(SERVER.channels, id=DELETEDMESSAGECHANNEL)
+    if message.channel != deleted_message_channel:
+        await deleted_message_channel.send(str(message.author) + ": " + message.content)
+        for attachment in message.attachments:
+            convert = await attachment.to_file(use_cached=True)
+            await deleted_message_channel.send(file=convert)
 
 
-bot.run('')
+bot.run(BOTTOKEN)
