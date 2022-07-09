44ffabd6f061adfccfc211a6b94931d81b1d721e
Alexander Liang <al5628@students.cs.ubc.ca>
2022-07-08 19:45:19 -0700

    add MySQL and add sample comamnd text

--- DBCharlie-Public.py
+++ DBCharlie-Public.py
@@ -1,29 +1,37 @@
 import json
 
 import discord
-import asyncio
-import random
+import mysql.connector
+import re
 from discord.ext.commands import bot
 
-
-
-REACTIONROLECHANNELID = None
+REACTIONROLECHANNELID = 0000000
 SERVERID = 0000000000
 WELCOMECHANNELID = 0000000
-DELETEDMESSAGECHANNEL = 000000000
+DELETEDMESSAGECHANNELID = 000000000
 BOTTOKEN = ""
 
-
-
-
-
-
 SERVER = None
+
+
+def connect_sql():
+    mydb = mysql.connector.connect(
+        host="host",
+        user="user",
+        password="password",
+        database='database'
+    )
+    return mydb
+
+
+def disconnect_sql(connection):
+    connection.disconnect()
 
 
 @bot.event
 async def on_ready():
     print('Logged in as {0} ({0.id})'.format(bot.user))
+    global SERVER
     SERVER = discord.utils.get(bot.guilds, id=SERVERID)
 
     # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="It's less broken"))
@@ -38,43 +46,41 @@ async def on_raw_reaction_add(payload):
 
         for roleid in custom_emoji_role_dict:
             if payload.emoji.id == roleid:
-                await payload.member.add_roles(discord.utils.get(SERVER.roles,name=custom_emoji_role_dict.get(roleid)))
+                await payload.member.add_roles(discord.utils.get(SERVER.roles, name=custom_emoji_role_dict.get(roleid)))
                 break
 
         builtin_emoji_role_dict = {"emoji name": "role name", "emoji name": "role name",
-                     "emoji name": "role name"}
+                                   "emoji name": "role name"}
 
         for roleid in builtin_emoji_role_dict:
             if payload.emoji.name == roleid:
-                await payload.member.add_roles(discord.utils.get(SERVER.roles,name=builtin_emoji_role_dict.get(roleid)))
+                await payload.member.add_roles(
+                    discord.utils.get(SERVER.roles, name=builtin_emoji_role_dict.get(roleid)))
                 break
-
-
-
 
 
 @bot.event
 async def on_member_join(member):
-
     channel = discord.utils.get(SERVER.channels, id=WELCOMECHANNELID)
-    await channel.send("")
-
+    await channel.send("Sample Welcome Message")
 
 
 @bot.event
 async def on_message(message):
     print('Message from {0.author}: {0.content}'.format(message))
 
+    if "Sample in-message trigger" in message.content.lower():
+        await message.channel.send("Sample in-message response")
+
+    if re.match("t.*e.*s.*t", message.content.lower()):
+        emoji_to_add = discord.utils.get(message.author.guild.emojis, id=878813655406370847)
+        await message.add_reaction(emoji_to_add)
+
     if message.content == "thisisastartupmessageandhopefullynoonetriggersit":
         embedVar = discord.Embed(title="This is where you obtain roles", description="", color=0x123456)
-        embedVar.add_field(name="Simply react using the colour you want and you will recieve that colour",
-                           value="Please note that the colours are listed in order of decreasing strength " +
-                                 "(i.e you will need to clear roles to obtain a colour on the right)",
+        embedVar.add_field(name="Simply react using the colour you want and you will receive that colour",
                            inline=False)
         await message.channel.send(embed=embedVar)
-
-
-
 
     if message.content.startswith("!create-command"):
         if message.author.id == SERVER.owner_id:
@@ -88,10 +94,9 @@ async def on_message(message):
             with open("commands.json", "w") as outfile:
                 outfile.write(json_commands)
             embedVar = discord.Embed(title="Command Created",
-            description="With trigger: " + processedKey + " and response: " + processedDefinition, color=0x123456)
+                                     description="With trigger: " + processedKey + " and response: " + processedDefinition,
+                                     color=0x123456)
             await message.channel.send(embed=embedVar)
-
-
 
     with open("commands.json", "r") as open_command:
         commands = json.load(open_command)
@@ -106,12 +111,33 @@ async def on_message(message):
                 await message.delete()
                 await message.channel.send(commandstd.get(key))
 
+    if message.content[0] == "!" or message.content.lower()[0] == "e":
+        mydb = connect_sql()
+        mycursor = mydb.cursor(buffered=True)
+        mycursor.execute("SELECT * FROM commands")
+        myresult = mycursor.fetchall()
+        for x in myresult:
+            if x[0] == message.content.lower():
+                await message.channel.send(x[1])
+        mycursor.close()
+        disconnect_sql(mydb)
+
+        mydb = connect_sql()
+        mycursor = mydb.cursor(buffered=True)
+        mycursor.execute("SELECT * FROM commandsThatDelete")
+        myresult = mycursor.fetchall()
+        for x in myresult:
+            if x[0] == message.content.lower():
+                await message.delete()
+                await message.channel.send(x[1])
+        mycursor.close()
+        disconnect_sql(mydb)
 
 
 @bot.event
 async def on_message_delete(message):
-    deleted_message_channel = discord.utils.get(SERVER.channels, id=DELETEDMESSAGECHANNEL)
-    if message.channel != deleted_message_channel:
+    if message.channel.id != DELETEDMESSAGECHANNELID:
+        deleted_message_channel = discord.utils.get(SERVER.channels, id=DELETEDMESSAGECHANNELID)
         await deleted_message_channel.send(str(message.author) + ": " + message.content)
         for attachment in message.attachments:
             convert = await attachment.to_file(use_cached=True)
